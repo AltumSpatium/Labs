d7b9f68bf6f32e192a12fed43750f988692e12dc
Alex Martynov <sinner.in.me11@gmail.com>
2016-11-04 19:46:24 +0300

    Added first lab on DMaDMS

---
+++ 5 semester/DMaDMS/Lab1/Lab1.sql
@@ -0,0 +1,57 @@
+CREATE SCHEMA Labs;
+
+DROP TABLE IF EXISTS country;
+DROP TABLE IF EXISTS organisation;
+DROP TABLE IF EXISTS aircraft;
+DROP TABLE IF EXISTS crew;
+DROP TABLE IF EXISTS route;
+DROP TABLE IF EXISTS flight;
+
+CREATE TABLE `Labs`.`country` (
+	`countryID` INT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(45) NOT NULL,
+	PRIMARY KEY (`countryID`));
+
+CREATE TABLE `Labs`.`organisation` (
+	`organisationID` INT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(45) NOT NULL,
+	PRIMARY KEY (`organisationID`));
+
+CREATE TABLE `Labs`.`aircraft` (
+	`planeID` INT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(45) NOT NULL,
+	PRIMARY KEY (`planeID`));
+
+CREATE TABLE `Labs`.`crew` (
+	`crewID` INT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(45) NOT NULL,
+	PRIMARY KEY (`crewID`));
+
+CREATE TABLE `Labs`.`route` (
+	`routeID` INT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(45) NOT NULL,
+	PRIMARY KEY (`routeID`));
+
+CREATE TABLE `Labs`.`flight` (
+	`flightID` INT NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`flightID`));
+
+ALTER TABLE organisation
+ADD countryID INT DEFAULT NULL,
+ADD FOREIGN KEY (countryID) REFERENCES country (countryID) ON UPDATE CASCADE;
+
+ALTER TABLE aircraft
+ADD organisationID INT DEFAULT NULL,
+ADD FOREIGN KEY (organisationID) REFERENCES organisation (organisationID) ON UPDATE CASCADE;
+
+ALTER TABLE crew
+ADD organisationID INT DEFAULT NULL,
+ADD FOREIGN KEY (organisationID) REFERENCES organisation (organisationID) ON UPDATE CASCADE;
+
+ALTER TABLE flight
+ADD routeID INT DEFAULT NULL,
+ADD FOREIGN KEY (routeID) REFERENCES route (routeID) ON UPDATE CASCADE,
+ADD crew INT DEFAULT NULL,
+ADD FOREIGN KEY (crew) REFERENCES crew (crewID) ON UPDATE CASCADE,
+ADD plane INT DEFAULT NULL,
+ADD FOREIGN KEY (plane) REFERENCES aircraft (planeID) ON UPDATE CASCADE;
