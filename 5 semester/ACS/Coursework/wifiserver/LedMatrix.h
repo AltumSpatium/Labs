7cbe0ac4fc9b83409b7770ac68e14e1e8127075b
Alex Martynov <sinner.in.me11@gmail.com>
2016-11-30 12:10:30 +0300

    Added font table and matrix setting

---
+++ 5 semester/ACS/Coursework/wifiserver/LedMatrix.h
@@ -0,0 +1,66 @@
+#ifndef ledmatrix_header
+#define ledmatrix_header
+
+//**************************************************************************************************
+
+#include "arduino.h"
+
+//**************************************************************************************************
+
+#define LEDMATRIX_CMD_NOOP         0x00
+#define LEDMATRIX_CMD_DIGIT0       0x01
+#define LEDMATRIX_CMD_DIGIT1       0x02
+#define LEDMATRIX_CMD_DIGIT2       0x03
+#define LEDMATRIX_CMD_DIGIT3       0x04
+#define LEDMATRIX_CMD_DIGIT4       0x05
+#define LEDMATRIX_CMD_DIGIT5       0x06
+#define LEDMATRIX_CMD_DIGIT6       0x07
+#define LEDMATRIX_CMD_DIGIT7       0x08
+#define LEDMATRIX_CMD_DECODEMODE   0x09
+#define LEDMATRIX_CMD_INTENSITY    0x0A
+#define LEDMATRIX_CMD_SCANLIMIT    0x0B
+#define LEDMATRIX_CMD_SHUTDOWN     0x0C
+#define LEDMATRIX_CMD_DISPLAYTEST  0x0F
+
+//..................................................................................................
+
+#define LEDMATRIX_FONT_WIDTH   5
+#define LEDMATRIX_FONT_HEIGHT  7
+
+//**************************************************************************************************
+
+class LedMatrix
+{
+  //................................................................................................
+
+private:
+
+  static const uint8_t PROGMEM font[];
+
+  uint8_t pin_cs;
+  uint8_t pin_din;
+  uint8_t pin_clk;
+  uint8_t mod_cnt;
+
+  //................................................................................................
+
+protected:
+
+  void SendCmd(uint8_t _command, uint8_t _argument);
+
+  //................................................................................................
+
+public:
+
+  LedMatrix(uint8_t _cs, uint8_t, _din, uint8_t _clk, uint8_t _cnt);
+  uint16_t Width(void);
+  uint16_t Height(void);
+
+  //................................................................................................
+};
+
+//**************************************************************************************************
+
+#endif
+
+//**************************************************************************************************
